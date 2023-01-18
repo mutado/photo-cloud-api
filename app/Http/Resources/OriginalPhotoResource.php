e223cbc1ddd76acbf63e55a3e6b7ca52e1c8a8c2
Nazar Potipaka <mutado.nzr@gmail.com>
2023-01-18 11:44:03 +0100

    Photo path change

--- app/Http/Resources/OriginalPhotoResource.php
+++ app/Http/Resources/OriginalPhotoResource.php
@@ -5,6 +5,8 @@ namespace App\Http\Resources;
 use App\Models\OriginalPhoto;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 /** @mixin OriginalPhoto */
 class OriginalPhotoResource extends JsonResource
@@ -16,10 +18,12 @@ class OriginalPhotoResource extends JsonResource
     public function toArray($request): array
     {
         $size = getimagesize(storage_path('app/' . $this->path));
+        $imageFullPath = Storage::disk('local')->path($this->path);
+        $image = Image::make($imageFullPath);
         return [
             'id' => $this->id,
             'user_id' => $this->user_id,
-            'path' => route('original-photos.download', $this->id),
+            'path' => route('photos.show', $this->id).'.'.$image->extension,
             'references' => PhotoReferenceResource::collection($this->whenLoaded('photoReferences')),
             'width' => $size[0],
             'height' => $size[1],
